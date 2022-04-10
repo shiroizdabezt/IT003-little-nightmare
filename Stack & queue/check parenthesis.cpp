5d6e9c645927d013efa899d0d69922841e2fca8e
shiroizdabezt <codykhoanguyen@gmail.com>
2022-04-10 20:28:53 +0700

    Create check parenthesis.cpp

---
+++ Stack & queue/check parenthesis.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std ;
+int main()
+{
+    string s ;
+    while(1) 
+    {
+        cin >> s ;
+        if ( s == "END " ) 
+            break;
+        stack<char> st ;
+
+        for ( char c : s ) 
+        {
+            if ( "[")
+        }
+    }
+}
